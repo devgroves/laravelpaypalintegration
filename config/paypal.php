3795d61b84a01893ca14325f9a42e2dcd00ccb97
Dhivyasathishkumar <dhivyathiruvenkadam@gmail.com>
2022-09-16 22:45:04 +0530

    secret and clientid changes

--- config/paypal.php
+++ config/paypal.php
@@ -1,7 +1,7 @@
 <?php 
 return [ 
-    'client_id' => env('PAYPAL_CLIENT_ID','AdPKNbrS4rBr87luLQRtV441lZCXTpYzUUAOj0ccwUweSjy1LwfVw03Z-_7B7FHyYOHJc3qTfphV3twm'),
-    'secret' => env('PAYPAL_SECRET','ENaMTr17_419pCJFIdZaC_AxGZs31DTWGzHpmZwDzsP78Ioo3bCKSdPHHlMvLtrcDlO-f0FL_t8QkgfT'),
+    'client_id' => env('PAYPAL_CLIENT_ID','xxx'),
+    'secret' => env('PAYPAL_SECRET','xyz'),
     'currency_type' => env('CURRENCY_TYPE','EUR'),
     'api_url' => env('API_URL','https://api-m.sandbox.paypal.com/'),
     'settings' => array(
